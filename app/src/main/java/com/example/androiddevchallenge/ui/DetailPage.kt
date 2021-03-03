be4ae8ff7b845777965c5fb134299f0fff1d1f31
七夜春树 <nishino8818@qq.com>
2021-03-03 13:03:09 +0800

    update spot less

--- app/src/main/java/com/example/androiddevchallenge/ui/DetailPage.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/DetailPage.kt
@@ -18,11 +18,11 @@ package com.example.androiddevchallenge.ui
 import android.util.Log
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
--- app/src/main/java/com/example/androiddevchallenge/ui/ListPage.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/ListPage.kt
@@ -17,14 +17,14 @@ package com.example.androiddevchallenge.ui
 
 import android.util.Log
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
